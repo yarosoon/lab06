cae9eb2956b9e1b045e76c49fe99304a4044c4e0
yarosoon <yaros.moon.ru@bk.ru>
2024-09-09 16:21:11 +0300

    hm

--- tests/test1.cpp
+++ tests/test1.cpp
@@ -1,25 +1,74 @@
-#include <gtest/gtest.h>
-#include <string>
-#include <ostream>
-#include <fstream>
+#include "Account.h"
+#include "Transaction.h"
 
-void print(const std::string& text, std::ostream& out) {
-    // Записываем текст в указанный поток
-    out << text;
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+class AccountMock : public Account {
+public:
+    AccountMock(int id, int balance) : Account(id, balance) {}
+    MOCK_CONST_METHOD0(GetBalance, int());
+    MOCK_METHOD1(ChangeBalance, void(int diff)); 
+    MOCK_METHOD0(Lock, void());
+    MOCK_METHOD0(Unlock, void());
+};
+
+class TransactionMock : public Transaction {
+public:
+    MOCK_METHOD3(Make, bool(Account& from, Account& to, int sum));
+};
+
+TEST(Account, Mock) {
+    AccountMock acc(1, 666);
+    EXPECT_CALL(acc, GetBalance()).Times(1);
+    EXPECT_CALL(acc, ChangeBalance(testing::_)).Times(2);
+    EXPECT_CALL(acc, Lock()).Times(2);
+    EXPECT_CALL(acc, Unlock()).Times(1);
+    acc.GetBalance();
+    acc.ChangeBalance(100); 
+    acc.Lock();
+    acc.ChangeBalance(100);
+    acc.Lock(); 
+    acc.Unlock();
 }
 
-TEST(Print, InFileStream)
-{
-  std::string filepath = "file.txt";
-  std::string text = "hello";
-  std::ofstream out{filepath};
+TEST(Account, SimpleTest) {
+    Account acc(1, 666);
+    EXPECT_EQ(acc.id(), 1);
+    EXPECT_EQ(acc.GetBalance(), 666);
+    EXPECT_THROW(acc.ChangeBalance(200), std::runtime_error);
+    EXPECT_NO_THROW(acc.Lock());
+    acc.ChangeBalance(200);
+    EXPECT_EQ(acc.GetBalance(), 866);
+    EXPECT_THROW(acc.Lock(), std::runtime_error);
+    EXPECT_NO_THROW(acc.Unlock());
+}
 
-  print(text, out);
-  out.close();
+TEST(Transaction, Mock) {
+    TransactionMock tr;
+    Account ac1(1, 100);
+    Account ac2(2, 300);
+    EXPECT_CALL(tr, Make(testing::_, testing::_, testing::_))
+    .Times(5);
+    tr.set_fee(200);
+    tr.Make(ac1, ac2, 200);
+    tr.Make(ac2, ac1, 300);
+    tr.Make(ac1, ac1, 0); 
+    tr.Make(ac1, ac2, -5); 
+    tr.Make(ac2, ac1, 50); 
+}
 
-  std::string result;
-  std::ifstream in{filepath};
-  in >> result;
-
-  EXPECT_EQ(result, text);
+TEST(Transaction, SimpleTest) {
+    Transaction tr;
+    Account ac1(1, 100);
+    Account ac2(2, 300);
+    tr.set_fee(10);
+    EXPECT_EQ(tr.fee(), 10);
+    EXPECT_THROW(tr.Make(ac1, ac2, 40), std::logic_error);
+    EXPECT_THROW(tr.Make(ac1, ac2, -5), std::invalid_argument);
+    EXPECT_THROW(tr.Make(ac1, ac1, 100), std::logic_error);
+    EXPECT_FALSE(tr.Make(ac1, ac2, 400));
+    EXPECT_FALSE(tr.Make(ac2, ac1, 300));
+    EXPECT_FALSE(tr.Make(ac2, ac1, 290));
+    EXPECT_TRUE(tr.Make(ac2, ac1, 150));
 }
